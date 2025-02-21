347c31b4a6e81d0543b22eedb9d4107f2bfcf001
evelaveta <iluhina794@gmail.com>
2025-02-21 21:47:56 +0300

    программа

---
+++ mpl/2/task2.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+with open('2.dat', 'r') as f:
+    N = 6
+    arrays = {}
+    for i in range(1, N + 1):
+        arrays[f'x_{i}'] = [float(j) for j in f.readline().split()]
+        arrays[f'y_{i}'] = [float(j) for j in f.readline().split()]
+
+def plotp(x, y, i):
+    plt.figure(figsize=(6, 4))
+    plt.plot(x, y, color='darkviolet', linewidth=2)
+    plt.title(f'Frame {i}')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+
+    plt.xlim(0, 16)
+    plt.ylim(-10, 12)
+
+    plt.minorticks_on()
+    plt.grid(which='major', color='gray', linewidth=1)
+    plt.grid(which='minor', color='lightgray', linestyle=':', linewidth=0.5)
+    plt.yticks(np.arange(-10, 13, 2))
+    minor_yticks = np.arange(-10, 12, 1)
+    plt.gca().set_yticks(minor_yticks, minor=True)
+
+    folpath = '/Users/evelaveta/Desktop/Programming/term4/mpl/2'
+    fname =f' frame_{i}.png'
+    fpath = os.path.join(folpath, fname)
+    plt.savefig(fpath, dpi=300)
+
+for i in range(1, N + 1):
+    x = arrays[f'x_{i}']
+    y = arrays[f'y_{i}']
+    plotp(x, y, i)
